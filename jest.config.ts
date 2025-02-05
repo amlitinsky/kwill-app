f485464fd60dd24360d98fba3254118f59cdb5f2
Alexander Litinsky <alex@kwill.app>
2025-02-04 21:43:06 -0800

    commented out jest file for now

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from '@jest/types';
-import dotenv from 'dotenv';
+// import type { Config } from '@jest/types';
+// import dotenv from 'dotenv';
 
-// Load the .env.test file
-dotenv.config({ path: '.env.test' });
+// // Load the .env.test file
+// dotenv.config({ path: '.env.test' });
 
-const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleDirectories: ['node_modules', '<rootDir>'],
-  testMatch: ['**/*.test.ts', '**/*.spec.ts', '**/tests/**/*.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  setupFiles: ['dotenv/config'],
-};
+// const config: Config.InitialOptions = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+//   moduleDirectories: ['node_modules', '<rootDir>'],
+//   testMatch: ['**/*.test.ts', '**/*.spec.ts', '**/tests/**/*.ts'],
+//   moduleNameMapper: {
+//     '^@/(.*)$': '<rootDir>/src/$1',
+//   },
+//   setupFiles: ['dotenv/config'],
+// };
 
-export default config;
+// export default config;
